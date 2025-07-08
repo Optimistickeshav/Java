673d3abd29053e2bdcc9c79fdc2402d73dca92b1
Keshav Verma <54373529+Optimistickeshav@users.noreply.github.com>
2025-07-09 01:36:38 +0530

    Add files via upload
    1st upload

---
+++ A01_Variable.java
@@ -0,0 +1,22 @@
+public class A01_Variable{
+    public static void main(String[] args){
+        System.out.println(5+8*1);
+
+        int a = 1;
+        byte b = 2;
+        short c = 3;
+        long d = 4;
+
+        System.out.println(c*d);
+/*
+* try writing
+* byte e = 128;
+* ERROR ??
+*
+*
+*
+* */
+
+    }
+
+}
